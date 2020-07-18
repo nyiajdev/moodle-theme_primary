f0f67466b401e3f07f0c58a55ef5154e5f48c7a8
nyiajdev <pa.nhia.lee@nyiaj.io>
2020-07-18 12:38:40 -0500

    cleanup some build warnings

--- layout/columns2.php
+++ layout/columns2.php
@@ -15,11 +15,11 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * A two column layout for the boost theme.
+ * A two column layout for the primary theme.
  *
- * @package   theme_boost
- * @copyright 2016 Damyon Wiese
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    theme_primary
+ * @copyright  2020 NYIAJ LLC
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
